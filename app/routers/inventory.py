c67ab7719aaa5ecbe15e10c1b828a4f1f7c819e9
YaPeL <elmaildejapel@gmail.com>
2023-05-31 19:55:18 -0300

    remove unnnecesary commit, better messages

--- app/routers/inventory.py
+++ app/routers/inventory.py
@@ -28,15 +28,13 @@ def increment_product_quantity(product_id: int, amount: int, db: Session = Depen
             inventory = db.query(Inventory).filter(Inventory.product_id == product_id).first()
             if inventory:
                 inventory.quantity += amount
-                db.commit()
-                db.refresh(inventory)
-                return {"message": f"Quantity incremented successfully. Current quantity: {inventory}"}
+                return {"message": f"Quantity incremented successfully. Current quantity: {inventory.quantity}"}
             else:
                 raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
     except SQLAlchemyError:
         db.rollback()
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail="An error occurred while incrementing the quantity.")
+                            detail=f"An error occurred while incrementing the quantity.")
 
 
 @router.put("/{product_id}/decrement/{amount}")
@@ -46,16 +44,15 @@ def decrement_product_quantity(product_id: int, amount: int, db: Session = Depen
             inventory = db.query(Inventory).filter(Inventory.product_id == product_id).first()
             if inventory:
                 inventory.quantity -= amount
-                if inventory.quantity > 0:
-                    db.commit()
-                    db.refresh(inventory)
-                    return {"message": f"Quantity decremented successfully. Current quantity: {inventory}"}
+                if inventory.quantity >= 0:
+                    return {"message": f"Quantity decremented successfully. Current quantity: {inventory.quantity}"}
                 else:
                     db.rollback()
-                    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Insufficient inventory")
+                    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                        detail=f"Insufficient inventory: {inventory.quantity}")
             else:
                 raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
     except SQLAlchemyError:
         db.rollback()
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail="An error occurred while decreasing the quantity.")
+                            detail=f"An error occurred while decreasing the quantity.")
